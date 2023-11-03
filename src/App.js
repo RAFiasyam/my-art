33a7b9a47ef6ba967d24745a0b2105acb4bcfc74
RAFiasyam <rafi.asyam23@gmail.com>
2023-11-03 13:40:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <section className='bg-[#0069B3] w-full h-screen fixed '>
+      <div className="overflow-y-scroll ">
+        <p className='w-[100%] text-slate-300 text-[1290%] absolute top-[-10%]  font-rubik  tracking-[-5px]'>TheScreen</p>
+        <p className='text-slate-300 text-[1160%] absolute top-[10%] font-rubik  tracking-[-5px]'>ThisisText</p>
+        <p className='text-slate-300 text-[1445%] absolute top-[26%] font-rubik tracking-[-5px] '>Willfull</p>
+        <p className='text-slate-300 text-[1285%] absolute top-[48%]  font-rubik tracking-[-5px] '>OurScreen</p>
+        <p className='text-slate-300 text-[525%] absolute top-[72%]  font-rubik tracking-[-5px] '>RememberThatYouAreAFool</p>
+        <p className='text-slate-300 text-[1985%] absolute top-[71%]  font-rubik tracking-[-5px] '>MyMind</p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
